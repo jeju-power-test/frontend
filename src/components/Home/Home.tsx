d99d17f9e312718d33994319c8b1c047ef1e2501
useonglee <useong0830@naver.com>
2022-11-17 16:26:27 +0900

    #10 feat: Layout component 제거

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-import { Layout } from '@components/common';
 import * as Style from './Home.style';
 
 function Home() {
   return (
-    <Layout>
+    <section>
       <Style.HomeQuestion>나의 제주력은 몇점?</Style.HomeQuestion>
       <Style.HomeTitle>제주력 테스트</Style.HomeTitle>
       <Image
@@ -12,10 +11,11 @@ function Home() {
         alt="홈페이지 테스트"
         width={320}
         height={251}
+        priority
       />
       <Style.StartButton variant="primary">시작하기</Style.StartButton>
       <Style.UserCountText>지금까지 000,000명이 참여했어요</Style.UserCountText>
-    </Layout>
+    </section>
   );
 }
 
