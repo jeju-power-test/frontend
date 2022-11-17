d6f8264bf7a5b6a2847561205327ace2ef00d8a5
Seunghwan <ysp1307@naver.com>
2022-11-18 05:35:39 +0900

    #32 feat: 공유하기 기능 구현

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -12,10 +12,12 @@ import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { submitAnswerAtom } from '@recoil/submitAnswer';
 
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 function Result() {
   const router = useRouter();
+  const userUrl = `https://monumental-madeleine-10eb3f.netlify.app/${router.asPath}`;
   const submitAnswer = useRecoilValue(submitAnswerAtom);
-
   const { submitTestResult, userIdentity } = useTestResult();
 
   const scrollFadeIn = {
@@ -71,7 +73,12 @@ function Result() {
             <Button variant="primary" onClick={() => router.push('/')}>
               테스트 다시 하기
             </Button>
-            <Button variant="secondary">친구에게 공유하기</Button>
+            <CopyToClipboard
+              text={userUrl}
+              onCopy={() => alert('링크가 복사되었습니다')}
+            >
+              <Button variant="secondary">친구에게 공유하기</Button>
+            </CopyToClipboard>
           </Style.ButtonWrapper>
         </>
       )}
