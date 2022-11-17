aa570943b99fae7d222419b9b5c0dc51690ad2d6
useonglee <useong0830@naver.com>
2022-11-18 01:21:28 +0900

    #22 feat: 테스트 api 관련 타입 추가

--- src/types/result.ts
+++ src/types/result.ts
@@ -2,3 +2,34 @@ export interface IUserSubmitData {
   id: number;
   answer: string;
 }
+
+export interface IResultAnswerContents {
+  id: number;
+  answer: string;
+  commentary: string;
+  correct: string;
+}
+
+export interface IUserAvatar {
+  image: string;
+  name: string;
+  score: number;
+}
+
+export interface IUserIdentity {
+  name: string;
+  identity: JSX.Element;
+}
+
+export interface IResultData {
+  avatar: IUserAvatar;
+  contents: IResultAnswerContents[];
+  places: [];
+}
+
+export interface IPlaces {
+  description: string;
+  image: string;
+  title: string;
+  url: string;
+}
