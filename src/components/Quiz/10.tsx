d115aa50a3a877b8e3078f66323da5f89ca6a511
Seunghwan <ysp1307@naver.com>
2022-11-18 00:37:47 +0900

    #3 feat: 보너스문제 UI 추가 및 수정

--- src/components/Quiz/10.tsx
+++ src/components/Quiz/10.tsx
@@ -1,13 +1,28 @@
 import * as Style from './Quiz.style';
 import { QUIZ_DATA } from '@constants/quiz';
 import QuizContents from './QuizContents/QuizContents';
-
+import Image from 'next/image';
 function Quiz10() {
   return (
     <>
       <Style.ImgContainer>
-        <Style.Img />
-        <Style.Img />
+        <Style.ImgBox>
+          <Image
+            src="/assets/images/bonus1.png"
+            alt="프로그래바 아이콘"
+            width={117}
+            height={148}
+          />
+        </Style.ImgBox>
+
+        <Style.ImgBox>
+          <Image
+            src="/assets/images/bonus2.png"
+            alt="프로그래바 아이콘"
+            width={148}
+            height={98}
+          />
+        </Style.ImgBox>
       </Style.ImgContainer>
       <QuizContents data={QUIZ_DATA[9]} />
     </>
