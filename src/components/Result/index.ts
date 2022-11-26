8b35547faed10f52c542b1ad58db309324b70b3f
useonglee <useong0830@naver.com>
2022-11-26 17:55:42 +0900

    #60 feat: 로딩 컴포넌트, Identity 컴포넌트 모듈화

--- src/components/Result/index.ts
+++ src/components/Result/index.ts
@@ -1,3 +1,5 @@
 export { default as Answer } from './Answer/Answer';
 export { default as Friend } from './Friend/Friend';
+export { default as Identity } from './Identity/Identity';
+export { default as AnalysisLoading } from './Loading/AnalysisLoading';
 export { default as Places } from './Places/Places';
